8d6c213d02853316188cc08275453765df362474
justinwk11 <justinwk11@gamil.com>
2021-06-24 19:56:29 +0900

    Async request
    get token logic added

--- module/http/async_request.py
+++ module/http/async_request.py
@@ -1,4 +1,27 @@
 import aiohttp
+import json
+from aiohttp.helpers import TOKEN
+import requests
+
+
+token = None
+
+def get_token():
+    global token
+    with open("/home/rnd01/workspace/cnc_analyzer/config/config_request_token.json") as jsonFile:
+        _conf = json.load(jsonFile)
+    URL = 'http://9.8.100.153:8080/auth/realms/cyservice/protocol/openid-connect/token'
+    data = {
+        'client_id' : _conf['client_id'],
+        'client_secret' : _conf['client_secret'],
+        'grant_type' : _conf['grant_type'],
+        'username' : _conf['username'],
+        'password' : _conf['password']
+    }
+    res = requests.post(URL, data=data)
+    if res.status_code == 200:
+        token = res.text
+        print("get token")
 
 async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_time, _cycle, _count):
     """Send request using HTTP protocol by asynchronously
@@ -16,12 +39,15 @@ async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_ti
     async with aiohttp.ClientSession() as session:
         _request_info = _url_head + _url_tail + "?"
         _request_param = "opCode=" + _opcode + "&startTime=" + _start_time + "&endTime=" + _end_time + "&cycleTime=" + str(_cycle) + "&count=" + str(_count)
+        if token == None:
+            get_token()
         try:
-            async with session.get(_request_info + _request_param) as response:
+            async with session.get(_request_info + _request_param, headers={'token':token}) as response:
                 if response.status == 200:
                     result = await response.read()
-                else:
-                    print(response.status)
+                    _decode = json.loads(result.decode('utf-8'))
+                    print(response.status, _decode)
+                        
         except aiohttp.ClientConnectionError as e:
             print("process_connection error", str(e))
 
@@ -38,8 +64,24 @@ async def send_loss_info(_url_head, _url_tail, _loss):
     async with aiohttp.ClientSession() as session:
         _request_info = _url_head + _url_tail + "?"
         _request_param = "loss=" + str(_loss)
+        if token == None:
+            get_token()        
         try:
-            async with session.get(_request_info + _request_param) as response:
+            async with session.get(_request_info + _request_param, headers={'token':token}) as response:
                 result = await response.read()
+                #print(result)
+                #_decode = await json.loads(result.decode('utf-8'))
+                #print(response.status, _decode)
+
+                # if _decode['errormessage'] == 'oauth.v2.TokenNotFound':
+                #     _token = get_token()
+                #     URL = "http://9.8.100.153:8081" + "/main/real-time-loss" + "?" + "loss=" + "0.001"
+                #     res = requests.get(URL, headers={'token':_token})
+                #     print(res)
+
+                # if response.status == 200:
+                #     print(result)
+                # elif response.status == 401:
+                #     print(result)
         except aiohttp.ClientConnectionError as e:
             print("loss_connection error", str(e))
