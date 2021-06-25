5d556cbc413c46c4ecbf0bd0a77a96b6d88fb790
justinwk11 <justinwk11@gamil.com>
2021-06-25 13:46:34 +0900

    get token & get refresh token updated

--- module/http/async_request.py
+++ module/http/async_request.py
@@ -2,26 +2,55 @@ import aiohttp
 import json
 from aiohttp.helpers import TOKEN
 import requests
+import time
 
 
 token = None
+refresh_token = None
+_conf = None
+with open("/home/rnd01/workspace/cnc_analyzer/config/config_request_token.json") as jsonFile:
+        _conf = json.load(jsonFile)
 
 def get_token():
+    """Get token from auth server to send loss & process info
+    """
+    print("get token ", time.strftime('%c', time.localtime((time.time()))))
     global token
-    with open("/home/rnd01/workspace/cnc_analyzer/config/config_request_token.json") as jsonFile:
-        _conf = json.load(jsonFile)
-    URL = 'http://9.8.100.153:8080/auth/realms/cyservice/protocol/openid-connect/token'
-    data = {
+    global refresh_token
+
+    URL = _conf['auth_server']
+    _data = {
         'client_id' : _conf['client_id'],
         'client_secret' : _conf['client_secret'],
-        'grant_type' : _conf['grant_type'],
+        'grant_type' : _conf['grant_password'],
         'username' : _conf['username'],
         'password' : _conf['password']
     }
-    res = requests.post(URL, data=data)
+    res = requests.post(URL, data=_data)
     if res.status_code == 200:
         token = res.text
-        print("get token")
+        refresh_token = res.json()['refresh_token']
+
+def get_refresh_token():
+    """Get refresh token from auth server when token authentication timeout
+    """
+    global token
+    global refresh_token
+
+    URL = _conf['auth_server']
+    _data = {
+        'client_id' : _conf['client_id'],
+        'client_secret' : _conf['client_secret'],
+        'grant_type' : _conf['grant_refresh'],
+        'refresh_token' : refresh_token
+    }
+    res = requests.post(URL, data=_data)
+    if res.status_code == 200:
+        token = res.text
+        refresh_token = res.json()['refresh_token']
+
+def get_new_token():
+    global token
 
 async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_time, _cycle, _count):
     """Send request using HTTP protocol by asynchronously
@@ -45,8 +74,13 @@ async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_ti
             async with session.get(_request_info + _request_param, headers={'token':token}) as response:
                 if response.status == 200:
                     result = await response.read()
+                elif response.status == 401:
+                    result = await response.read()
                     _decode = json.loads(result.decode('utf-8'))
-                    print(response.status, _decode)
+                    if _decode['hnerrorcode'] == 404:
+                        get_refresh_token()
+                else:
+                    print("unknown error")
                         
         except aiohttp.ClientConnectionError as e:
             print("process_connection error", str(e))
@@ -68,20 +102,15 @@ async def send_loss_info(_url_head, _url_tail, _loss):
             get_token()        
         try:
             async with session.get(_request_info + _request_param, headers={'token':token}) as response:
-                result = await response.read()
-                #print(result)
-                #_decode = await json.loads(result.decode('utf-8'))
-                #print(response.status, _decode)
+                if response.status == 200:
+                    result = await response.read()
+                elif response.status == 401:
+                    result = await response.read()
+                    _decode = json.loads(result.decode('utf-8'))
+                    if _decode['hnerrorcode'] == 404:
+                        get_refresh_token()
+                else:
+                    print("unknown error")
 
-                # if _decode['errormessage'] == 'oauth.v2.TokenNotFound':
-                #     _token = get_token()
-                #     URL = "http://9.8.100.153:8081" + "/main/real-time-loss" + "?" + "loss=" + "0.001"
-                #     res = requests.get(URL, headers={'token':_token})
-                #     print(res)
-
-                # if response.status == 200:
-                #     print(result)
-                # elif response.status == 401:
-                #     print(result)
         except aiohttp.ClientConnectionError as e:
             print("loss_connection error", str(e))
