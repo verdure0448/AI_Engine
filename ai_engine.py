cf8c9cb5f90020b4d27363ab49aecfa659ce2c2e
justinwk11 <justinwk11@gamil.com>
2021-07-06 14:08:41 +0900

    Rest call path and logic changed
    update able to call number of process

--- ai_engine.py
+++ ai_engine.py
@@ -1,6 +1,7 @@
 import module.process.preprocessing as preprocessing
 import module.process.trend as trend
-import module.process.prediction as prediction
+import module.process.prediction as prediction_bi
+import module.process.prediction_cnn as prediction_conv2d
 
 from multiprocessing import Process, Event
 
@@ -12,13 +13,93 @@ sig_preprocess = Event()
 sig_trend = Event()
 sig_prediction = Event()
 
+sig_prediction_1 = Event()
+sig_prediction_2 = Event()
+
 preprocess_state = False
 trend_state = False
 prediction_state = False
 
+prediction_1_state = False
+prediction_2_state = False
+
 preprocessing_process = None
 trend_process = None
 prediction_process = None
+
+prediction_1_process = None
+prediction_2_process = None
+
+
+def bi_lstm_management(_status):
+    """
+    """
+    global sig_preprocess, sig_trend, sig_prediction
+    global preprocess_state, trend_state, prediction_state
+
+    if _status == True and preprocess_state == False and trend_state == False and prediction_state == False:
+        preprocess_management(_status)
+        trend_management(_status)
+        prediction_management(_status)
+
+        return True
+    elif _status == False and preprocess_state == True and trend_state == True and prediction_state == True:
+        preprocess_management(_status)
+        trend_management(_status)
+        prediction_management(_status)
+
+        return True
+
+    elif _status == "info" and preprocess_state == True and trend_state == True and prediction_state == True:
+        _preprocess_info = preprocess_management(_status)
+        _trend_info = trend_management(_status)
+        _prediction_info = prediction_management(_status)
+
+        _result = []
+        _result.append(_preprocess_info)
+        _result.append(_trend_info)
+        _result.append(_prediction_info)
+
+        return _result
+
+    else:
+        return False
+
+
+def conv2d_lstm_management(_status):
+    """
+    """
+    global sig_preprocess, sig_trend, sig_prediction
+    global preprocess_state, trend_state, prediction_state
+
+    if _status == True and preprocess_state == False and trend_state == False and prediction_state == False:
+        preprocess_management(_status)
+        trend_management(_status)
+        prediction_management(_status)
+
+        return True
+    elif _status == False and preprocess_state == True and trend_state == True and prediction_state == True:
+        preprocess_management(_status)
+        trend_management(_status)
+        prediction_management(_status)
+
+        return True
+
+    elif _status == "info" and preprocess_state == True and trend_state == True and prediction_state == True:
+        _preprocess_info = preprocess_management(_status)
+        _trend_info = trend_management(_status)
+        _prediction_info = prediction_management(_status)
+
+        _result = []
+        _result.append(_preprocess_info)
+        _result.append(_trend_info)
+        _result.append(_prediction_info)
+
+        return _result
+
+    else:
+        return False
+
 
 def process_management(_status):
     """To manage entire process
@@ -149,31 +230,44 @@ def trend_management(_status):
     else:
         return False
 
-def prediction_management(_status):
-    """To manage prediction process
-    Prediction process start if _status True and stop all if _status false
-
-    Args:
-        _status (bool): True is start, False is stop
-
-    Returns:
-        bool: True is start or stop, False is nothing happen
-    """
+_prediction_list = []
+def prediction_management(_status, _model=None, _numb=None):
     global sig_prediction
     global prediction_state
-    global prediction_process
+    prediction_process = "prediction_process_{}"
 
     if _status == True and prediction_state == False:
-        prediction_state = True
-        prediction_process = Process(
-            name='prediction',
-            target=prediction.run,
-            args=(sig_prediction,)
-        )
-        sig_prediction.clear()
-        prediction_process.start()
+        
+        if _model == "bi":
+            for i in range(_numb):
+                globals()[prediction_process.format(i)] = Process(
+                    name="prediction_bi",
+                    target=prediction_bi.run,
+                    args=(sig_prediction,)
+                )
+                globals()[prediction_process.format(i)].start()
+                _prediction_list.append(globals()[prediction_process.format(i)])
+            sig_prediction.clear()
+            prediction_state = True
 
-        return True
+            return True
+
+        elif _model == "conv2d":
+            for i in range():
+                globals()[prediction_process.format(i)] = Process(
+                    name="prediction_conv2d",
+                    target=prediction_conv2d.run,
+                    args=(sig_prediction,)
+                )
+                globals()[prediction_process.format(i)].start()
+                _prediction_list.append(globals()[prediction_process.format(i)])
+            sig_prediction.clear()
+            prediction_state = True
+
+            return True
+        
+        else:
+            return False
 
     elif _status == False and prediction_state == True:
         prediction_state = False
@@ -182,16 +276,21 @@ def prediction_management(_status):
         return True
 
     elif _status == "info" and prediction_state == True:
-        _result = {
-            "name" : prediction_process.name,
-            "pid" : prediction_process.pid,
-            "alive" : prediction_process.is_alive()
-        }
+        _result = []
+        print(len(_prediction_list))
+        print(_prediction_list)
+        for i in range(len(_prediction_list)):
+            _result.append({
+                "name" : _prediction_list[i].name,
+                "pid" : _prediction_list[i].pid,
+                "alive" : _prediction_list[i].is_alive()
+            })
 
         return _result
-    
+
     else:
         return False
+
 
 app = Flask(__name__)
 api = Api(app, version='0.1',
@@ -200,6 +299,12 @@ api = Api(app, version='0.1',
     terms_url="/",
     contact="justinwk11@hncorp.world",
     license="Apache License 2.0"
+)
+
+model_api = api.namespace(
+    name='processes of model',
+    path='/model',
+    description='Process of model API'
 )
 
 process_api = api.namespace(
@@ -232,6 +337,7 @@ wrong_request_stop = {
     'State' : 'Nothing to stop process. please start process first'
 }
 
+
 @process_api.route('/start_all')
 class ProcessStart(Resource):
     @process_api.doc('Whole Process Start')
@@ -252,14 +358,12 @@ class ProcessStop(Resource):
         else:
             return wrong_request_stop
 
-
 @process_api.route('/info_all')
 class ProcessInfo(Resource):
     @process_api.doc('whole Process Info')
     def get(self):
         _process_control = process_management("info")
         return _process_control
-
 
 @preprocess_api.route('/start')
 class PreprocessStart(Resource):
@@ -312,7 +416,6 @@ class TrendEnd(Resource):
         else:
             return wrong_request_stop
 
-
 @trend_api.route('/info')
 class TrendInfo(Resource):
     @trend_api.doc('Trend Process Status')
@@ -320,14 +423,13 @@ class TrendInfo(Resource):
         _process_control = trend_management("info")
         return _process_control
 
-
-@prediction_api.route('/start')
-class PredictionStart(Resource):
+@prediction_api.route('/<model>/<int:num_process>')
+class PredicttionStart(Resource):
     @prediction_api.doc('Prediction Process Start')
-    def get(self):
-        _process_control = prediction_management(True)
+    def get(self, model, num_process):
+        _process_control = prediction_management(True, model, num_process)
         if _process_control == True:
-            return {'State' : 'Prediction start'}
+            return {'State' : 'Prediction Start'}
         else:
             return wrong_request_start
 
@@ -351,5 +453,8 @@ class PredictionInfo(Resource):
 
 
 if __name__ == "__main__":
-    process_management(True)
+    preprocess_management(True)
+    trend_management(True)
+    prediction_management(True, "bi", 4)
+    
     app.run(debug=False, host='9.8.100.151', port=8080)
--- module/http/async_request.py
+++ module/http/async_request.py
@@ -45,9 +45,6 @@ def get_refresh_token():
         'refresh_token' : refresh_token
     }
     res = requests.post(URL, data=_data)
-    print(res)
-    print(res.status_code)
-    print(res.text)
     if res.status_code == 200:
         token = res.text
         refresh_token = res.json()['refresh_token']
@@ -80,11 +77,7 @@ async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_ti
                 if response.status == 200:
                     result
                 elif response.status == 401:
-                    print("[401] in send_process_info")
-                    print(_decode)
-                    print(_decode['hnerrorcode'])
                     if _decode['hnerrorcode'] == "E004":
-                        print("hello")
                         get_refresh_token()
                     elif _decode['hnerrorcode'] == "E003":
                         get_token()
@@ -93,7 +86,6 @@ async def send_process_info(_url_head, _url_tail , _opcode, _start_time, _end_ti
 
                 elif response.status == 403:
                     if _decode['hnerrorcode'] == "E005":
-                        #print('[403, E005]', result)
                         get_token()
                     else:
                         print('[403, Unknown]', result)
@@ -123,21 +115,17 @@ async def send_loss_info(_url_head, _url_tail, _loss):
                 result = await response.read()
                 _decode = json.loads(result.decode('utf-8'))
                 if response.status == 200:
-                    #print('[200]', result)
                     result
                 elif response.status == 401:
                     if _decode['hnerrorcode'] == "E004":
-                        #print('[401, E004]', result)
                         get_refresh_token()
                     elif _decode['hnerrorcode'] == "E003":
-                        #print('[401, E003]', result)
                         get_token()
                     else:
                         print('[401, Unknown]', result)
 
                 elif response.status == 403:
                     if _decode['hnerrorcode'] == "E005":
-                        #print('[403, E005]', result)
                         get_token()
                     else:
                         print('[403, Unknown]', result)
@@ -146,17 +134,3 @@ async def send_loss_info(_url_head, _url_tail, _loss):
 
         except aiohttp.ClientConnectionError as e:
             print("loss_connection error", str(e))
-
-"""async def send_detection_info(_predict):
-    async with aiohttp.ClientSession() as session:
-        _request_info = "http://9.8.100.153:8082/detection" + "?"
-        _request_param = "info=" + str(_predict)
-
-        try:
-            async with session.get(_request_info + _request_param) as response:
-                if response.status == 200:
-                    result = await response.read()
-                    print(result)
-        except aiohttp.ClientConnectionError as e:
-            print("send_detection error")
-            """
